f12e45edd261979a5c1dd4edf9ac33152d84755a
Rathod Jagadish <46041625+Jagadishrathod@users.noreply.github.com>
2022-05-22 18:03:07 +0530

    Create Looping_And_Skipping.sh

---
+++ Looping_And_Skipping.sh
@@ -0,0 +1,46 @@
+'
+Your task is to use for loops to display only odd natural numbers from 1 to 100.
+
+Input Format
+
+There is no input.
+
+Constraints
+
+-
+
+Output Format
+
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+Sample Input
+
+-
+
+Sample Output
+
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+Explanation
+
+-
+'
+#Solution
+for ((i=1; i<100; i=i+2))
+do 
+    echo $i
+done
