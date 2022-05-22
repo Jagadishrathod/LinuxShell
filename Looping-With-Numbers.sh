06bdcea49ef75419ea371353fe0984e1b97360f1
Rathod Jagadish <46041625+Jagadishrathod@users.noreply.github.com>
2022-05-22 17:57:55 +0530

    Create Looping-With-Numbers.sh

---
+++ Looping-With-Numbers.sh
@@ -0,0 +1,27 @@
+'
+Use a for loop to display the natural numbers from  to .
+
+Input Format
+
+There is no input
+
+Output Format
+
+1
+2
+3
+4
+5
+.
+.
+.
+.
+.
+50
+'
+
+#Solution
+for ((i=1; i<51; i++));
+do
+    echo $i;
+done;
